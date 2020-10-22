bb3650511c45ea0244dad472e8b3e0157c2a6d46
wenisumardi <wenipatungga12@gmail.com>
2020-10-22 21:42:37 +0700

    Update index.blade.php
    mengubah button checkout menjadi button di navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -66,7 +66,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						  <a class="toggleMenu" href="#"><img src="images/nav.png" alt="" /></a>
 						    <ul class="nav" id="nav">
 						    	<li><a href="{{ url ("/shop") }}">Shop</a></li>
-						 		<li><a href="{{ url ("/contact") }}">Contact</a></li>								
+						 		<li><a href="{{ url ("/contact") }}">Contact</a></li>
+						 		<li><a href="{{ url ("/checkout") }}">Checkout</a></li>								
 								<div class="clear"></div>
 							</ul>
 							<script type="text/javascript" src="js/responsive-nav.js"></script>
@@ -102,7 +103,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 						  <li class="list_img"><img src="images/1.jpg" alt=""/></li>
 						  <li class="list_desc"><h4><a href="#">Nama Pengguna</a></h4></li>
 						  <div class="login_buttons">
-							 <div class="check_button"><a href="{{ url ("/checkout") }}">Check out</a></div>
 							 <div class="login_button"><a href="{{ url ("/login") }}">Login</a></div>
 							 <div class="clear"></div>
 						  </div>
